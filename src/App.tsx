958f2e4aa4776348ab4769d66f16cc15d6825d93
Amel Umesh <45140781+Amel294@users.noreply.github.com>
2024-12-03 08:41:23 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Board from "./components/board/board"
+
+const App = () => {
+  return (
+    <div>
+         <Board/>     
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/board/Board.tsx
@@ -0,0 +1,12 @@
+
+const Board = () => {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold underline">
+      Board
+    </h1>
+    </div>
+  )
+}
+
+export default Board
