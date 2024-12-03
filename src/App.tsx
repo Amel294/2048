d8a7243d49a91715bd9edd18e515732658b4c3ff
Amel Umesh <45140781+Amel294@users.noreply.github.com>
2024-12-03 11:51:28 +0530

    style : changed style to bring the board to center of the page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Board from "./components/board/Board"
 
 const App = () => {
   return (
-    <div className="bg-black">
+    <div className="bg-black min-h-screen p-0 m-0 flex items-center justify-center">
          <Board/>     
     </div>
   )
