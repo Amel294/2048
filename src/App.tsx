9a7e657daf6815cb05186fdc6dab957f7a051083
Amel Umesh <45140781+Amel294@users.noreply.github.com>
2024-12-03 12:17:30 +0530

    feat :Added Title component and wrapped Board and Title component in a div with flex layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import Board from "./components/board/Board"
+import Title from "./components/Title/Title"
 
 const App = () => {
   return (
     <div className="bg-black min-h-screen p-0 m-0 flex items-center justify-center">
-         <Board/>     
+      <div className="flex flex-col items-center ">
+
+      <Title />
+      <Board />
+      </div>
     </div>
   )
 }
---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,6 @@
+function Title() {
+  return (
+    <div className="flex flex-col items-center text-8xl bg-gradient-to-b from-white to-black text-transparent bg-clip-text ">2048</div>
+  )
+}
+export default Title
